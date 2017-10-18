f1da7420578dcf70158384c32f8281ae1bade9ad
Shreya Bharadwaj <shreyab@nrifintech.com>
2017-10-18 11:35:21 +0530

    Fetched latest changes of components

--- components/gid-api/assets/js/gidRestApi.js
+++ components/gid-api/assets/js/gidRestApi.js
@@ -1,75 +1,104 @@
 var apiPath = {
-		  "lobs":{
-			  "default": "/lobs" 
-		  },
-		  "concepts": {
-			"default": "/lobs/{lob}/domains"
-		  },
-		  "applications": {
-		    "default": "/domains/{concept}/applications",
-		    "all": "/applications"
-		  },
-		  "databases": {
-		    "default": "/domains/{concept}/applications/{application}/databases",
-		    "all": "/applications/{application}/databases"
-		  },
-		  "schemas": {
-		    "default": "/domains/{concept}/applications/{application}/databases/{database}/schemas",
-		    "all": "/applications/{application}/databases/{database}/schemas"
-		  },
-		  "tables": {
-		    "default": "/domains/{concept}/schemas/{schema}/tables",
-		    "all": "/schemas/{schema}/tables"
-		  },
-		  "columns": {
-			  "table": {
-			   "default":"/domains/{concept}/tables/{table}/columns",
-			   "all":"/tables/{table}/columns"
-			  },
-			  "schema": {
-			   "default" :"/domains/{concept}/schemas/{schema}/columns",
-			   "all":"/schemas/{schema}/columns"
-			  },
-			  "database": {
-			   "default" :"/domains/{concept}/applications/{application}/databases/{database}/columns",
-			   "all":"/applications/{application}/databases/{database}/columns"
-			  },
-			  "application": {
-			   "default": "/domains/{concept}/applications/{application}/columns",
-			   "all" : "/applications/{application}/columns"
-			  }
-		  },
-		  "get-samples": {
-			 "default": "/samples"
-		  },
-		  "recommended-concepts": {
-			 "default": "/columns/{column}/domains"
-		  },
-		  "verify-concept": {
-			  "default": "/columns/{column}/domains/{concept}"
-		  },
-		  "list-concepts": {
-			  "default": "/domains"
-		  },
-		  "concept-details":{
-			  "default": "/domains/{concept}"
-		  },
-		  "curate-concept": {
-			  "default": "/domains/{concept}/curation"
-		  },
-		  "create-concept": {
-			  "default": "/domains"
-		  },
-		  "edit-concept": {
-			  "default": "/domains/{concept}"
-		  },
-		  "remove-concept": {
-			  "default": "/domains/{concept}"
-		  },
-		  "auth": {
-			  "default": "/api/security/api-token-v1"
-		  },
-		  "faceted-columns": {
-			  "default": "/columns"
-		  }
-		};
+    "lobs": {
+        "default": "/lobs"
+    },
+    "concepts": {
+        "default": "/lobs/{lob}/domains"
+    },
+    "applications": {
+        "default": "/domains/{concept}/applications",
+        "all": "/applications"
+    },
+    "databases": {
+        "default": "/domains/{concept}/applications/{application}/databases",
+        "all": "/applications/{application}/databases"
+    },
+    "schemas": {
+        "default": "/domains/{concept}/applications/{application}/databases/{database}/schemas",
+        "all": "/applications/{application}/databases/{database}/schemas"
+    },
+    "tables": {
+        "default": "/domains/{concept}/schemas/{schema}/tables",
+        "all": "/schemas/{schema}/tables"
+    },
+    "columns": {
+        "table": {
+            "default": "/domains/{concept}/tables/{table}/columns",
+            "all": "/tables/{table}/columns"
+        },
+        "schema": {
+            "default": "/domains/{concept}/schemas/{schema}/columns",
+            "all": "/schemas/{schema}/columns"
+        },
+        "database": {
+            "default": "/domains/{concept}/applications/{application}/databases/{database}/columns",
+            "all": "/applications/{application}/databases/{database}/columns"
+        },
+        "application": {
+            "default": "/domains/{concept}/applications/{application}/columns",
+            "all": "/applications/{application}/columns"
+        }
+    },
+    "get-samples": {
+        "default": "/samples"
+    },
+    "recommended-concepts": {
+        "default": "/columns/{column}/domains"
+    },
+    "verify-concept": {
+        "default": "/columns/{column}/domains/{concept}"
+    },
+    "list-concepts": {
+        "default": "/domains"
+    },
+    "concept-details": {
+        "default": "/domains/{concept}"
+    },
+    "curate-concept": {
+        "default": "/domains/{concept}/curation"
+    },
+    "create-concept": {
+        "default": "/domains"
+    },
+    "edit-concept": {
+        "default": "/domains/{concept}"
+    },
+    "remove-concept": {
+        "default": "/domains/{concept}"
+    },
+    "auth": {
+        "default": "/api/security/api-token-v1"
+    },
+    "faceted-columns": {
+        "default": "/columns"
+    },
+    "database-types": {
+        "default": "/databases/types"
+    },
+    "add-connection": {
+        "default": "/databases"
+    },
+    "list-connections": {
+        "default": "/databases"
+    },
+    "referrals": {
+        "flag": {
+        	"default": "/referrals"
+        },
+        "unflag": {
+        	"default":  "/referrals/{referral}"
+        },
+        "list": {
+        	"default":  "/referrals"
+        },
+        "reject":  {
+        	"default": "/referrals/{referral}"
+        },
+        "accept":  {
+        	"default": "/referrals/{referral}"
+        }
+    },
+    "dispositions": {
+        "default": "/referrals/dispositions"
+    }
+};
